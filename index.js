e5ce7907be11f311800109d1bc0250b9af3d5c72
Blake Kunishige <blake3457@yahoo.com>
2018-01-30 04:15:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {
   kittens.push('Ralph')
 }
+var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyPrependKitten(name) {
   kittens.unshift('Bob')
 }
+var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyRemoveLastKitten() {
   kittens.pop()
 }
+var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
 }
-
+var kittens = ["Milo", "Otis", "Garfield"]
 function appendKitten(name) {
   
 }
